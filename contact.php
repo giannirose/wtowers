c5d8e6304cde4ee04f37308734885d6e9109baeb
John Rose <john.rose3@comcast.net>
2020-12-29 08:03:40 -0800

    contact.php
    This is from the live site, filed directly under public_html, and includes the Wufoo ActionGram form

---
+++ contact.php
@@ -0,0 +1,35 @@
+<?php include('perch/runtime.php');?>
+<?php perch_layout('global.header'); ?>
+<div class="page_intro">
+	<?php perch_content('Contact Form Introduction'); ?>
+		<!--class="page_intro" is a simple textarea in the admin area-->
+		<!--the contact_wt.html form is in templates > forms-->
+	&nbsp;
+	&nbsp;
+</div>
+			
+	<!--begin Wufoo Action Gram form -->
+<div id="wufoo-z1rpn8zv1dv8h5m">
+	Fill out my <a href="https://wallowers.wufoo.com/forms/z1rpn8zv1dv8h5m">online form</a>.
+</div>
+<div id="wuf-adv" style="font-family:inherit;font-size: small;color:#a7a7a7;text-align:center;display:block;">There are tons of <a href="http://www.wufoo.com/features/">Wufoo features</a> to help make your forms awesome.
+</div>
+<script type="text/javascript">var z1rpn8zv1dv8h5m;(function(d, t) {
+var s = d.createElement(t), options = {
+'userName':'wallowers',
+'formHash':'z1rpn8zv1dv8h5m',
+'autoResize':true,
+'height':'617',
+'async':true,
+'host':'wufoo.com',
+'header':'show',
+'ssl':true};
+s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.com/scripts/embed/form.js';
+s.onload = s.onreadystatechange = function() {
+var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
+try { z1rpn8zv1dv8h5m = new WufooForm();z1rpn8zv1dv8h5m.initialize(options);z1rpn8zv1dv8h5m.display(); } catch (e) {}};
+var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
+})(document, 'script');</script>
+<!-- End Wufoo Action Gram form --> 			
+
+<?php perch_layout('global.footer'); ?>
