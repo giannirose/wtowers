530d45a0ad339ed739f597faf43bedfd7d44739d
John Rose <john.rose3@comcast.net>
2020-12-30 08:05:01 -0800

    Create policy.php
    Copied from live site

---
+++ pages/policy.php
@@ -0,0 +1,11 @@
+<?php if (!defined('PERCH_RUNWAY')) include($_SERVER['DOCUMENT_ROOT'].'/perch/runtime.php'); ?>
+	<?php perch_layout('head');?>
+
+		<?php perch_layout('header'); ?>
+	
+			<?php perch_content('Policy'); ?>
+				<p class="text_right">Review the <a href="index.php">full list of Policies</a>.
+				</p>
+					<p class="text_right">View the <a href="../documents/by-laws.php">Willamette Towers By-Laws</a>.
+					</p>
+	<?php perch_layout('global.footer');?>
