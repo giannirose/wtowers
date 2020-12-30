2a800c4356a867df124b2f763b7d32e9c1cc1f69
John Rose <john.rose3@comcast.net>
2020-12-30 07:58:54 -0800

    Create home-page-item.php
    Page from live site

---
+++ pages/home-page-item.php
@@ -0,0 +1,6 @@
+<?php include($_SERVER['DOCUMENT_ROOT'].'/perch/runtime.php'); ?>
+<?php perch_layout('global.header');?>
+
+<?php perch_content('Home Page Item'); ?>
+
+<?php perch_layout('global.footer');?>
