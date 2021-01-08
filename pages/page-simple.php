46d3602d34a0ae9145877f5311057ba592f66c49
John Rose <john.rose3@comcast.net>
2021-01-08 08:19:51 -0800

    Create page-simple.php

---
+++ pages/page-simple.php
@@ -0,0 +1,4 @@
+<?php include($_SERVER['DOCUMENT_ROOT'].'/perch/runtime.php'); ?>
+	<?php perch_layout('global.header');?>
+		<?php perch_content('Simple Page'); ?>
+			<?php perch_layout('global.footer');?>
