02ec5345bb9d737b9d02efe4229404147adaab4e
John Rose <john.rose3@comcast.net>
2021-01-15 06:58:32 -0800

    Update poll-internet.php
    Use simple page format and add Wufoo poll script

--- pages/poll-internet.php
+++ pages/poll-internet.php
@@ -1,6 +1,7 @@
+<?php include($_SERVER['DOCUMENT_ROOT'].'/perch/runtime.php'); ?>
+	<?php perch_layout('global.header');?>
+		<?php perch_content('Simple Page'); ?>
 
+  <div id="wufoo-wtsd2hx070numt"> Fill out my <a href="https://wallowers.wufoo.com/forms/wtsd2hx070numt">online form</a>. </div> <script type="text/javascript"> var wtsd2hx070numt; (function(d, t) { var s = d.createElement(t), options = { 'userName':'wallowers', 'formHash':'wtsd2hx070numt', 'autoResize':true, 'height':'396', 'async':true, 'host':'wufoo.com', 'header':'show', 'ssl':true }; s.src = ('https:' == d.location.protocol ?'https://':'http://') + 'secure.wufoo.com/scripts/embed/form.js'; s.onload = s.onreadystatechange = function() { var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return; try { wtsd2hx070numt = new WufooForm(); wtsd2hx070numt.initialize(options); wtsd2hx070numt.display(); } catch (e) { } }; var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr); })(document, 'script'); </script>
 
-
-
-
-<div id="wufoo-wtsd2hx070numt"> Fill out my <a href="https://wallowers.wufoo.com/forms/wtsd2hx070numt">online form</a>. </div> <script type="text/javascript"> var wtsd2hx070numt; (function(d, t) { var s = d.createElement(t), options = { 'userName':'wallowers', 'formHash':'wtsd2hx070numt', 'autoResize':true, 'height':'396', 'async':true, 'host':'wufoo.com', 'header':'show', 'ssl':true }; s.src = ('https:' == d.location.protocol ?'https://':'http://') + 'secure.wufoo.com/scripts/embed/form.js'; s.onload = s.onreadystatechange = function() { var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return; try { wtsd2hx070numt = new WufooForm(); wtsd2hx070numt.initialize(options); wtsd2hx070numt.display(); } catch (e) { } }; var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr); })(document, 'script'); </script>
+			<?php perch_layout('global.footer');?>
