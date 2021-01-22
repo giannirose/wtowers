5a26f426ad1518e847f6c8882c2472539b985c2a
John Rose <john.rose3@comcast.net>
2021-01-22 07:12:30 -0800

    Create wt-reach.php
    wt-reach.php contains the perch_content_custom details that is used to display office contact details entered on the listing page _office_reach.html

---
+++ pages/wt-reach.php
@@ -0,0 +1,10 @@
+<?php include('perch/runtime.php');?>
+<?php perch_layout('global.header');?>
+
+<?php perch_content_custom('WT Hours & Other Contact Info', array(
+		'page'=>'/reach.php',
+		'template'=>'_office_reach.html'
+	));
+	?>
+
+<?php perch_layout('global.footer');?>
