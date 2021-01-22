3522c22f0d496eeb2e9fbf5c5b07b91a11ff5784
John Rose <john.rose3@comcast.net>
2021-01-22 07:07:23 -0800

    Create reach.php
    From local files, reach.php is the basic template for information that may be used throughout the site. Editor can enter information here that is programmed to appear on pages where necessary using perch_custom

---
+++ pages/reach.php
@@ -0,0 +1,11 @@
+<?php include('perch/runtime.php');?>
+<?php perch_layout('global.header');?>
+<h3>Board Members</h3>
+<?php perch_content('Board Members'); ?>
+
+<?php perch_content('WT Hours & Other Contact Info'); ?>
+
+<h3>Subheadings</h3>
+<?php perch_content('subheadings'); ?>
+
+<?php perch_layout('global.footer');?>
