68dc3940c466f17d3f6d2c4d942551845f138571
John Rose <john.rose3@comcast.net>
2021-08-07 05:47:51 -0700

    Create head.php
    Version prior to live site update 2021_08_06

---
+++ layouts/head.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset=utf-8>
+		<title><?php perch_pages_title(); ?> | Willamette Towers</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<?php perch_page_attributes(); ?>
+			<?php perch_get_css(); ?>
+			<?php perch_get_javascript(); ?>
+				<script type="text/javascript"></script>
+	</head>
+    <body >		
