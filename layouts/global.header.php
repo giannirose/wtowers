7710130e7edd8c08c13d50b19138934e9923f11d
John Rose <john.rose3@comcast.net>
2021-08-07 05:45:09 -0700

    Update global.header.php
    Version prior to live site update 2021_08_06

--- layouts/global.header.php
+++ layouts/global.header.php
@@ -7,23 +7,23 @@
 			<?php perch_page_attributes(); ?>
 			<?php perch_get_css(); ?>
 				<script type="text/javascript"></script>
-    			<script> src="js/jquery-1.11.1.min.js"</script>	
+ <!--   			<script> src="js/jquery-1.11.1.min.js"</script>	 Checking if the site will work without downloading jquery -->
 	</head>
-    <body >
-	<div class="page"> <!--div class="page" is closed in the global footer-->
-		<div class="headerbar"> <!--begin header-->
-			<header> 
-				<hgroup>
-					<h1>Willamette Towers</h1>	
-						<perch:if exists="subhead">	
-							<h2><perch:content id="sub_subhead" type="text" label="Sub-headline for page header" order="2"/></h2>	
-						</perch:if>
-				</hgroup>
-					<nav> 
-						<ul>
-							<li><a href="/index.php" >Menu Page</a></li> 
-						</ul>
-					</nav>					
-			</header>
-		</div> <!-- end header -->
-	<div class="border"><!--adds border around all content following the header- not used on the index.php page. This div is closed in the global.footer-->
+   <body >
+		<div class="page wrapper"> <!--div class="page" is closed in the global footer-->
+			<div class="headerbar"> <!--begin header-->
+				<header> 
+					<hgroup>
+						<h1 class="header-head">Willamette Towers</h1>	
+							<perch:if exists="subhead">	
+								<p class="header-subhead"><perch:content id="sub_subhead" type="text" label="Sub-headline for page header" order="2"/></p>	
+							</perch:if>
+					</hgroup>
+						<nav> 
+							<ul>
+								<li><a href="/index.php" >Menu Page</a></li> 
+							</ul>
+						</nav>					
+				</header>
+			</div> <!-- end div class="headerbar" -->
+				<div class="border"><!--adds border around all content following the header- not used on the index.php page. This div is closed in the global.footer-->
