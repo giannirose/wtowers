7dc62e45898a3631275ccdd1adef9fe4dd998557
John Rose <john.rose3@comcast.net>
2021-08-07 05:44:00 -0700

    Create global.header-plain.php
    Version prior to live site update 2021_08_06

---
+++ layouts/global.header-plain.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset=utf-8>
+		<title><?php perch_pages_title(); ?></title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<?php perch_page_attributes(); ?>
+			<?php perch_get_css(); ?>
+				<script type="text/javascript"></script>
+ <!--   			<script> src="js/jquery-1.11.1.min.js"</script>	-->
+	</head>
+   <body >
+		<div class="page wrapper"> <!--div class="page" is closed in the global footer-->
+			<div class="headerbar"> <!--begin header-->
+				<header> 
+					<hgroup>
+						<p class="header-head">Willamette Towers</p>	
+					</hgroup>				
+				</header>
+		</div> <!-- end header -->
+	<div class="plain border"><!--adds border around all content following the header- not used on the index.php page. This div is closed in the global.footer-->
