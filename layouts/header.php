ff94e97e3ac0ff560a28506a69eee8b06cab7e0c
John Rose <john.rose3@comcast.net>
2021-08-07 05:49:04 -0700

    Create header.php
    Version prior to live site update 2021_8_06

---
+++ layouts/header.php
@@ -0,0 +1,12 @@
+	<div class="page"> <!--div class="page" is closed in the global footer-->	
+		<div class="headerbar"> <!--begin header-->
+			<header> 
+				<h1 class="header-head">Willamette Towers</h1>
+					<nav> 
+						<ul>
+							<li><a href="/index.php">Menu Page</a></li> 
+						</ul>
+					</nav>					
+			</header>
+		</div> <!-- end headerbar -->
+	<div class="border"><!--adds border around all content following the header- not used on the index.php page. Closed in the global.footer-->
